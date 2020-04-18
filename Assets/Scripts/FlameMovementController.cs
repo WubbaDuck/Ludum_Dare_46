ab54469d834e60fb52a5d32d1beec6a153e1f1cf
WubbaDuck <kd8gby@gmail.com>
2020-04-18 18:19:54 -0400

    Added initial files. Basic movement and pickups working.

---
+++ Assets/Scripts/FlameMovementController.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameMovementController : MonoBehaviour
+{
+    public float maxMoveSpeed = 5f;
+    public float acceleration = 10f;
+    public float slideAcceleration = 20f;
+    public float gravity = -9.81f;
+    public float jumpStrength = 6f;
+    public float jumpCanceledGravityMultiplier = 6f;
+
+    public LayerMask wallsMask;
+    public LayerMask ceilingsMask;
+    public LayerMask platformsMask;
+
+    private float horizontalMovementRaw;
+    private float verticalMovementRaw;
+    private Vector2 newMovement;
+    private Vector3 velocity;
+    private bool jumping = false;
+    private bool isOnGround = false;
+    private bool jumpCanceled = false;
+    private float colliderSizeX;
+    private float colliderSizeY;
+    private bool platformDrop = false;
+    private float platformDropCooldown = 0.3f;
+
+    private RaycastHit2D lastRayHitResult = new RaycastHit2D();
+
+    void Start()
+    {
+        colliderSizeX = GetComponent<BoxCollider2D>().size.x / 2f;
+        colliderSizeY = GetComponent<BoxCollider2D>().size.y / 2f;
+    }
+
+    void Update()
+    {
+        bool canJump = false;
+
+        horizontalMovementRaw = Input.GetAxisRaw("Horizontal"); // Get the horizontal movement
+        verticalMovementRaw = Input.GetAxisRaw("Vertical"); // Get the vertial movement
+
+        if (horizontalMovementRaw != 0) // Player is inputing movement commands
+        {
+            if (velocity.x == 0 || Mathf.Sign(horizontalMovementRaw) == Mathf.Sign(velocity.x))
+            {
+                velocity.x += horizontalMovementRaw * acceleration * Time.deltaTime;
+            }
+            else
+            {
+                velocity.x += horizontalMovementRaw * slideAcceleration * Time.deltaTime;
+            }
+        }
+        else if (velocity.x != 0) // Not movement commands but the character is still moving
+        {
+            velocity.x -= Mathf.Sign(velocity.x) * acceleration * Time.deltaTime;
+
+            // Zero the velocity if close enough to zero
+            if (velocity.x < 0.1f && velocity.x > -0.1f)
+            {
+                velocity.x = 0;
+            }
+        }
+
+        // Jumping
+        canJump = GetJumpAvailability();
+
+        if (verticalMovementRaw >= 0 && isOnGround && canJump)
+        {
+            isOnGround = false;
+            velocity.y = jumpStrength;
+        }
+
+        // Cancel jump if falling
+        if (Mathf.Sign(velocity.y) < 0)
+        {
+            jumpCanceled = false;
+        }
+
+        // Handle platform dropdown
+        if(!platformDrop && verticalMovementRaw < 0 && isOnGround && canJump)
+        {
+            StartCoroutine(DropThroughPlatform());
+        }
+
+        // Clamp the velocity
+        velocity.x = Mathf.Clamp(velocity.x, -maxMoveSpeed, maxMoveSpeed);
+        velocity.y = Mathf.Clamp(velocity.y, -maxMoveSpeed, maxMoveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += velocity * Time.fixedDeltaTime; // Move the charactedir, ColliderSize, maskr;
+
+        // Mmmm Gravity
+        if (!isOnGround)
+        {
+            if (jumpCanceled)
+            {
+                velocity.y += gravity * Time.fixedDeltaTime * jumpCanceledGravityMultiplier;
+
+            }
+            else
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+        }
+
+        // Check collisions
+        CollisionDetectionDown(!platformDrop);
+        CollisionDetectionWalls();
+        CollisionDetectionCeiling();
+    }
+
+    private IEnumerator DropThroughPlatform()
+    {
+        platformDrop = true;
+        isOnGround = false;
+        yield return new WaitForSeconds(platformDropCooldown);
+        platformDrop = false;
+    }
+
+    private bool GetJumpAvailability()
+    {
+        bool buttonDown = Input.GetButton("Jump");
+
+        if (jumping && !buttonDown)
+        {
+            jumping = false;
+            jumpCanceled = true;
+        }
+
+        if (isOnGround && buttonDown && !jumping)
+        {
+            jumping = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool Raycast(Vector2 direction, float diststance, LayerMask mask)
+    {
+        lastRayHitResult = Physics2D.Raycast(transform.position, direction, diststance, mask);
+        // Debug.DrawRay(transform.position, direction, Color.red, Time.deltaTime);
+        if (lastRayHitResult.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void CollisionDetectionDown(bool checkAll = true)
+    {
+        bool collisionDetected = false;
+
+        if (velocity.y <= 0)
+        {
+            if (checkAll)
+            {
+                collisionDetected = Raycast(-transform.up, colliderSizeY, (wallsMask | ceilingsMask | platformsMask));
+            }
+            else
+            {
+                collisionDetected = Raycast(-transform.up, colliderSizeY, (wallsMask | ceilingsMask));
+            }
+
+        }
+
+        if (collisionDetected)
+        {
+            transform.position = new Vector3(transform.position.x, lastRayHitResult.point.y + colliderSizeY, transform.position.z);
+            velocity.y = 0;
+            isOnGround = true;
+        }
+        else
+        {
+            isOnGround = false;
+        }
+    }
+
+    private void CollisionDetectionWalls()
+    {
+        bool collisionDetected = false;
+
+        if (velocity.x < 0) // Check left if we are moving left
+        {
+            collisionDetected = Raycast(-transform.right, colliderSizeX, (wallsMask));
+
+            if (collisionDetected)
+            {
+                transform.position = new Vector3(lastRayHitResult.point.x + colliderSizeX, transform.position.y, transform.position.z);
+                velocity.x = 0;
+            }
+        }
+        else if (velocity.x > 0) // Check right if we are moving right
+        {
+            collisionDetected = Raycast(transform.right, colliderSizeX, (wallsMask));
+
+            if (collisionDetected)
+            {
+                transform.position = new Vector3(lastRayHitResult.point.x - colliderSizeX, transform.position.y, transform.position.z);
+                velocity.x = 0;
+            }
+        }
+    }
+
+    private void CollisionDetectionCeiling()
+    {
+        bool collisionDetected = false;
+
+        if (velocity.y >= 0)
+        {
+            collisionDetected = Raycast(transform.up, colliderSizeY, (ceilingsMask));
+        }
+
+        if (collisionDetected)
+        {
+            transform.position = new Vector3(transform.position.x, lastRayHitResult.point.y - colliderSizeY, transform.position.z);
+            velocity.y = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/FuelHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelHandler : MonoBehaviour
+{
+    public float secondsPerFuelUsed = 1f;
+    public LayerMask fuelMask;
+    public LayerMask damageMask;
+
+    private float fuelLevel = 25f;
+    
+    void Start()
+    {
+        InvokeRepeating("BurnFuel", secondsPerFuelUsed * 3f, secondsPerFuelUsed);
+    }
+
+    void Update()
+    {
+        // Check for burnout
+        if(fuelLevel <= 0)
+        {
+            BurnOut();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(LayerMask.GetMask(LayerMask.LayerToName(col.gameObject.layer)) == fuelMask.value)
+        {
+            fuelLevel += col.gameObject.GetComponent<FuelPickup>().fuelAmount;
+            Destroy(col.gameObject);
+        }
+        else if(LayerMask.GetMask(LayerMask.LayerToName(col.gameObject.layer)) == damageMask.value)
+        {
+            fuelLevel -= col.gameObject.GetComponent<FuelPickup>().fuelAmount;
+            Destroy(col.gameObject);
+        }
+        Debug.Log(fuelLevel);
+    }
+
+    private void BurnFuel()
+    {
+        fuelLevel--;
+        Debug.Log(fuelLevel);
+    }
+
+    private void BurnOut()
+    {
+        Destroy(gameObject);
+    }
+
+    public float GetCurrentFuelLevel()
+    {
+        return fuelLevel;
+    }
+}
