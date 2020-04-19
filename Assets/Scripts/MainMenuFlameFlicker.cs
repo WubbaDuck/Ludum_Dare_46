a44929b55df6d476f02fa6718d6cf783bddbdb2a
WubbaDuck <kd8gby@gmail.com>
2020-04-19 00:18:28 -0400

    Main Menu added

---
+++ Assets/Scripts/MainMenuFlameFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFlameFlicker : MonoBehaviour
+{
+    public float flickerRate = 0.2f;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D flameLight;
+    private bool flickerTick = false;
+    private float baseIntensity;
+    private FuelHandler fuelHandler;
+
+    void Start()
+    {
+        flameLight = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        baseIntensity = flameLight.intensity;
+        InvokeRepeating("Flicker", 0, flickerRate);
+    }
+
+    void Flicker()
+    {
+        if(flickerTick)
+        {
+            float max = flameLight.intensity * 1.20f;
+            float min = flameLight.intensity * 0.80f;
+            flameLight.intensity = Random.Range(min, max);
+            flickerTick = false;
+        }
+        else
+        {
+            flameLight.intensity = baseIntensity;
+            flickerTick = true;
+        }
+    }
+
+    void UpdateOuterRadius()
+    {
+        // flameLight.pointLightOuterRadius = fuelHandler.GetCurrentFuelLevel()/2;
+    }
+}
