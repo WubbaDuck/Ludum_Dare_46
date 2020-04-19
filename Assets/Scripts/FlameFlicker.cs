39c601fca3d483cfd8b1db164c9991e60f6f121b
WubbaDuck <kd8gby@gmail.com>
2020-04-18 22:36:03 -0400

    Added flame light flicker and light decrease over time.

---
+++ Assets/Scripts/FlameFlicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameFlicker : MonoBehaviour
+{
+    public float flickerRate = 0.2f;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D flameLight;
+    private bool flickerTick = false;
+    private float baseIntensity;
+    private FuelHandler fuelHandler;
+
+    void Start()
+    {
+        flameLight = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        fuelHandler = GetComponent<FuelHandler>();
+        baseIntensity = flameLight.intensity;
+        InvokeRepeating("Flicker", 0, flickerRate);
+        InvokeRepeating("UpdateOuterRadius", 0, flickerRate);
+    }
+
+    void Flicker()
+    {
+        if(flickerTick)
+        {
+            float max = flameLight.intensity * 1.20f;
+            float min = flameLight.intensity * 0.80f;
+            flameLight.intensity = Random.Range(min, max);
+            flickerTick = false;
+        }
+        else
+        {
+            flameLight.intensity = baseIntensity;
+            flickerTick = true;
+        }
+    }
+
+    void UpdateOuterRadius()
+    {
+        flameLight.pointLightOuterRadius = fuelHandler.GetCurrentFuelLevel()/2;
+    }
+}
